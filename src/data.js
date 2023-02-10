14cf305651f0d3a5253057b41c3f9d8947cd07d3
SvetLana203 <shirapovauu@gmail.com>
2023-02-10 14:38:23 -0600

    hooks2

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    text: "Hero of uot time"
+  },
+
+  {
+    id: 2,
+    text: "Nothing lasts forever"
+  },
+
+  {
+    id: 3,
+    text: "The naked face"
+  },
+
+  {
+    id: 4,
+    text: "Angel's flight"
+  },
+
+  {
+    id: 5,
+    text: "Sex and the City"
+  }
+]
